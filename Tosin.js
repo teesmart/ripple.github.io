56f0b5b6f063c1e579f7366a971379e0a1226250
teesmart <42120584+teesmart@users.noreply.github.com>
2019-09-08 17:49:13 +0100

    Add files via upload

---
+++ Tosin.js
@@ -0,0 +1,23 @@
+<script>
+function contactform()
+{
+var name=document.forms["form"]["name"];
+var E-mail=document.forms["form"]["E-mail"];
+var password=document.forms["forms"]["password"];
+
+if (inputs[index].value==null || inputs[index].value=="" || inputs[i     alert("Field is not 4 characters minimum!");     return false;
+
+if (name.value==""){window.alert("please enter name");
+name.focus();
+return false;
+}
+if (E.mail.value==""){window.alert("please enter email");
+	E.mail.focus();
+	return false;}
+if(password.vaue==""){window.alert("please enter password");
+password.focus();
+return false;
+}
+return true;
+}
+</script>
